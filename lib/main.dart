69f4c8f8fd260fe0a3502e5dd24cc470ba1e6467
DanyHttp <graywolfali@gmail.com>
2024-11-10 00:52:44 +0330

    add increament

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Home());}
-
+  runApp(const Home());
+}
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false,
-    home: MyWidget());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyWidget(),
+    );
   }
 }
 
-class MyWidget extends StatelessWidget {
+class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
+
+  @override
+  _MyWidgetState createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int _count = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _count++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,8 +38,18 @@ class MyWidget extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.blue.shade300,
         title: const Text("Weather App"),
-
       ),
+      body: Center(
+        child: Text(
+          'Count: $_count',
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: const Icon(Icons.add_business),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
