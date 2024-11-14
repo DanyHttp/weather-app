3ae488b9ffa6dcf882de2987df4214a193f015a2
DanyHttp <graywolfali@gmail.com>
2024-11-14 17:46:29 +0330

    ubg

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Home());
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyWidget(),
-    );
-  }
-}
-
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  _MyWidgetState createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  int _count = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _count++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue.shade300,
-        title: const Text("Weather App"),
-      ),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
       body: Center(
-        child: Text(
-          'Count: $_count',
-          style: const TextStyle(fontSize: 24),
-        ),
+        child: Text('Hi dude'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add_business),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
-  }
+    ),
+  ));
 }
